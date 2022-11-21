edd03c33c37fb5c277517272dc279395656228a9
Hwajun1323 <whajun1323@gmail.com>
2022-11-21 15:08:45 +0100

    mouse drawing

--- app.js
+++ app.js
@@ -2,5 +2,27 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(50, 50, 100, 200);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(800, 800);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
