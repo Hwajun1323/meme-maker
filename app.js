55cdf12135d971162eb511c04608d424d7e380dd
Hwajun1323 <whajun1323@gmail.com>
2022-11-22 09:52:40 +0100

    paint color

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
   );
-const color = document.getElementById("color");
-const lineWidth = document.getElementById("line-width");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = lineWidth.value;
-let isPainting = false;
-
-function onMove(event) {
-  if (isPainting) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    return;
+  const color = document.getElementById("color");
+  const lineWidth = document.getElementById("line-width");
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 800;
+  ctx.lineWidth = lineWidth.value;
+  let isPainting = false;
+  
+  function onMove(event) {
+    if (isPainting) {
+      ctx.lineTo(event.offsetX, event.offsetY);
+      ctx.stroke();
+      return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
   }
-  ctx.moveTo(event.offsetX, event.offsetY);
-}
-function startPainting() {
-  isPainting = true;
-}
-function cancelPainting() {
-  isPainting = false;
-  ctx.beginPath();
-}
-function onLineWidthChange(event) {
-  ctx.lineWidth = event.target.value;
-}
-
-function onColorChange(event) {
+  function startPainting() {
+    isPainting = true;
+  }
+  function cancelPainting() {
+    isPainting = false;
+    ctx.beginPath();
+  }
+  function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+  }
+  
+  function onColorChange(event) {
     ctx.strokeStyle = event.target.value;
     ctx.fillStyle = event.target.value;
   }
-
+  
   function onColorClick(event) {
     const colorValue = event.target.dataset.color;
     ctx.strokeStyle = colorValue;
     ctx.fillStyle = colorValue;
     color.value = colorValue;
   }
-
-canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting);
-
-lineWidth.addEventListener("change", onLineWidthChange);
-color.addEventListener("change", onColorChange);
-
-colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+  
+  canvas.addEventListener("mousemove", onMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", cancelPainting);
+  canvas.addEventListener("mouseleave", cancelPainting);
+  
+  lineWidth.addEventListener("change", onLineWidthChange);
+  color.addEventListener("change", onColorChange);
+  
+  colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
