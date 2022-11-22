451bb53a2cf179653df8804f7976a1d9a116f32a
Hwajun1323 <whajun1323@gmail.com>
2022-11-22 13:30:20 +0100

    filling mode

--- app.js
+++ app.js
@@ -1,52 +1,72 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
-    document.getElementsByClassName("color-option")
-  );
-  const color = document.getElementById("color");
-  const lineWidth = document.getElementById("line-width");
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = 800;
-  canvas.height = 800;
-  ctx.lineWidth = lineWidth.value;
-  let isPainting = false;
-  
-  function onMove(event) {
-    if (isPainting) {
-      ctx.lineTo(event.offsetX, event.offsetY);
-      ctx.stroke();
-      return;
-    }
-    ctx.moveTo(event.offsetX, event.offsetY);
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
   }
-  function startPainting() {
-    isPainting = true;
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
   }
-  function cancelPainting() {
-    isPainting = false;
-    ctx.beginPath();
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
   }
-  function onLineWidthChange(event) {
-    ctx.lineWidth = event.target.value;
-  }
-  
-  function onColorChange(event) {
-    ctx.strokeStyle = event.target.value;
-    ctx.fillStyle = event.target.value;
-  }
-  
-  function onColorClick(event) {
-    const colorValue = event.target.dataset.color;
-    ctx.strokeStyle = colorValue;
-    ctx.fillStyle = colorValue;
-    color.value = colorValue;
-  }
-  
-  canvas.addEventListener("mousemove", onMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", cancelPainting);
-  canvas.addEventListener("mouseleave", cancelPainting);
-  
-  lineWidth.addEventListener("change", onLineWidthChange);
-  color.addEventListener("change", onColorChange);
-  
-  colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
